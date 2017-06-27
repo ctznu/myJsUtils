f633312c4d0f72e583df00b57277a331461ee3de
ctznu <ctzn2010@qq.com>
2017-06-27 22:55:47 +0800

    js string  util
    a js string util

---
+++ utils/mergeValString.js
@@ -0,0 +1,36 @@
+function mergeValString(desStr, compStr, splitStr, isUnion) {
+    if (desStr && !compStr) {
+        return desStr;
+    } else if (!desStr && compStr) {
+        return isUnion ? compStr : desStr;
+    } else if (desStr && compStr) {
+        splitStr = splitStr || ',';
+        var desStrArr = desStr.split(splitStr);
+        var compStrArr = compStr.split(splitStr);
+        compStrArr.map(function (val) {
+            if(isUnion){
+                if(desStr.indexOf(val) == -1) {
+                    desStrArr.push(val);
+                }
+            }else{
+                if(desStrArr.indexOf(val) > -1){
+                    desStrArr.splice(desStrArr.indexOf(val), 1);
+                }
+            }
+
+
+        });
+        return desStrArr.join(splitStr);
+
+    }
+}
+
+// var desStr = 'ab,cd,ef';
+var desStr = null;
+// var compStr = 'cd,ss';
+var compStr = null;
+var splitStr = ',';
+
+mergeValString(desStr, compStr, splitStr, true);
+mergeValString(desStr, compStr, splitStr, false);
+
